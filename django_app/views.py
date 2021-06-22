05b5009fc8f2cb11b388f9b335d0d1d1042a4064
Ryan Pilon <ryanpilon1995@gmail.com>
2021-06-22 16:08:08 -0700

    test complete up and running

--- django_app/views.py
+++ django_app/views.py
@@ -1,3 +1,10 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def test_view(request):
+    data = {
+        "name": "test",
+        "age": 13,
+    }
+    return JsonResponse(data)
