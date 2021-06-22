4204260ef35dd43df4d26239d8dee20a255c9b87
Ryan Pilon <ryanpilon1995@gmail.com>
2021-06-22 16:21:26 -0700

    Test view created with djangoframework package

--- django_app/views.py
+++ django_app/views.py
@@ -1,10 +1,22 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+#3rd party imports
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-def test_view(request):
-    data = {
-        "name": "test",
-        "age": 13,
-    }
-    return JsonResponse(data)
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "name": "test",
+            "age": 13,
+        }
+        return Response(data)
+
+# def test_view(request):
+#     data = {
+#         "name": "test",
+#         "age": 13,
+#     }
+#     return JsonResponse(data)
